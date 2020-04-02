ffa8740ecf5424fc06290b0b7fdaa1396aa27b7a
Briseid Pagador <bpagador@gmail.com>
2020-04-01 17:27:11 -0700

    wrote checkResult function

--- checkResult.js
+++ checkResult.js
@@ -1,7 +1,11 @@
-if (player === computer) {
-    return 0;
-} else if (player < computer) {
-    return -1;
-} else if (player > computer) {
-    return 1;
+function checkResult(player, computer) {
+    if (player === computer) {
+        return 0;
+    } else if (player < computer) {
+        return -1;
+    } else if (player > computer) {
+        return 1;
+    }
 }
+
+export default checkResult; 
