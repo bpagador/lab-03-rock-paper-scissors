c7f3f9efc81440fd3de9bf2fc8f5937fe42b0add
Briseid Pagador <bpagador@gmail.com>
2020-04-01 18:13:37 -0700

    most up to date lab, needs completion/polish

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 import getRandomThrow from '/.get-random-throw.js';
 import checkResult from '/.check-result.js';
 
+const playButton = document.getElementById('play-button');
+const computerDraw = document.getElementById('computer-draw');
+const winCount = document.getElementById('wins');
+const lossCount = document.getElementById('losses');
+const totalGames = document.getElementById('total-games');
 
-// need DOM elements
 //radio buttons with the users throw choice 
+
+ 
 //button the use click for playing
+
 //elements needed to display throw (win, lose, draw)
+
+
 //current wins losses and draws count
+
 
 //app state
 // wins
