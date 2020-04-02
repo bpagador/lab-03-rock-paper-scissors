f0a4f7e1045f2974c41fd06cea05bf623d5720c6
Briseid Pagador <bpagador@gmail.com>
2020-04-01 18:10:57 -0700

    started app.js construction, needs completion/polish

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 import getRandomThrow from '/.get-random-throw.js';
+import checkResult from '/.check-result.js';
+
+
+// need DOM elements
+//radio buttons with the users throw choice 
+//button the use click for playing
+//elements needed to display throw (win, lose, draw)
+//current wins losses and draws count
+
+//app state
+// wins
+//losses
+// draw
+
+//event listeners
+
 
